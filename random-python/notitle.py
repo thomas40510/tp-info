476117569ba7f6de4c51bb4213360d41423a9b67
thomas40510 <thomasprevost85@gmail.com>
2017-12-28 08:02:30 +0100

    commit

--- random-python/notitle.py
+++ random-python/notitle.py
@@ -2,12 +2,13 @@ import random as rd
 
 alpha='azertyuiopqsdfghjklmwxcvbn'
 
-def shakespeare(n):
+def shakespeare(n,t):
     t=''
     l=[]
     for i in range(n):
         c = rd.randrange(26)
         l.append(alpha[c])
     return(t.join(l))
+    #return (l.count(t))
 
 print(shakespeare(1000000).count("hello"))
