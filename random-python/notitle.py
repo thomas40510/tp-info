d0f0363ae004b3b367dc8249a52d4c2af0f914ad
thomas40510 <thomasprevost85@gmail.com>
2018-03-12 21:37:06 +0100

    commit

--- random-python/notitle.py
+++ random-python/notitle.py
@@ -2,7 +2,7 @@ import random as rd
 
 alpha='azertyuiopqsdfghjklmwxcvbn'
 
-def shakespeare(n,t):
+def shakespeare(n):
     t=''
     l=[]
     for i in range(n):
@@ -11,4 +11,23 @@ def shakespeare(n,t):
     return(t.join(l))
     #return (l.count(t))
 
-print(shakespeare(1000000).count("hello"))
+a = 0
+while a==0:
+    s = shakespeare(100000)
+    print(s)
+    a = s.count("hamlet")
+print(a)
+#print(shakespeare(1000000).count("hello"))
+
+def pgcd():
+    a,b = 0,0
+    p = 1
+    while a>=b:
+        a = int(input('entrer a :'))
+        b = int(input('entrer b :'))
+    for i in range(1,a+1):
+        if a%i==0 and b%i==0:
+            p = i
+    return p
+
+print('pgcd =',pgcd())
