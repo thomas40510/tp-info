f7855ac1a84a831bb5e47f4e23f6327f2f8dd05b
thomas40510 <thomasprevost85@gmail.com>
2019-10-10 22:22:02 +0200

    add .gitignore some modifs

--- random-python/notitle.py
+++ random-python/notitle.py
@@ -1,49 +1,45 @@
 import random as rd
+import numpy as np
+import matplotlib.pyplot as plt
 
-alpha='azertyuiopqsdfghjklmwxcvbn'
+alpha = 'azertyuiopqsdfghjklmwxcvbn'
+
 
 def shakespeare(n):
-    t=''
-    l=[]
+    t = ''
+    l = []
     for i in range(n):
         c = rd.randrange(26)
         l.append(alpha[c])
     print(t.join(l))
-    return(t.join(l))
-    #return (l.count(t))
+    return (t.join(l))
+    # return (l.count(t))
+
+
 def search(word):
     a = 0
-    i=0
-    while a==0:
+    i = 0
+    while a == 0:
         s = shakespeare(10000)
         print(s)
         a = s.count(word)
-        i+=1
-    print(i*10000)
+        i += 1
+    print(i * 10000)
     print(a)
 
-#print(shakespeare(1000000).count("hamlet"))
+
+# print(shakespeare(1000000).count("hamlet"))
 
 def pgcd():
-    a,b = 0,0
+    a, b = 0, 0
     p = 1
-    while a>=b:
+    while a >= b:
         a = int(input('entrer a :'))
         b = int(input('entrer b :'))
-    for i in range(1,a+1):
-        if a%i==0 and b%i==0:
+    for i in range(1, a + 1):
+        if a % i == 0 and b % i == 0:
             p = i
     return p
 
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
 
-x = car.keys()
-
-print(x)
-
-
-#print('pgcd =',pgcd())
+# print('pgcd =',pgcd())
